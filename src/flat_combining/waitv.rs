945f97903ed1e992ba72ac2438510ab6f2c4a2bb
Alex Gaynor <alex.gaynor@gmail.com>
2025-06-29 16:31:41 -0400

    format

--- src/flat_combining/waitv.rs
+++ src/flat_combining/waitv.rs
@@ -29,12 +29,16 @@ pub fn wait_mutex_or_waiter<'a, T>(mutex: &'a Mutex<T>, waiter: &Waiter) -> Wait
             }
         } else if mutex_value == Mutex::<T>::LOCKED {
             // Upgrade the mutex to contended.
-            if mutex.futex().compare_exchange_weak(
-                Mutex::<T>::LOCKED,
-                Mutex::<T>::CONTENDED,
-                Ordering::Relaxed,
-                Ordering::Relaxed,
-            ).is_err() {
+            if mutex
+                .futex()
+                .compare_exchange_weak(
+                    Mutex::<T>::LOCKED,
+                    Mutex::<T>::CONTENDED,
+                    Ordering::Relaxed,
+                    Ordering::Relaxed,
+                )
+                .is_err()
+            {
                 continue;
             }
             mutex_value = Mutex::<T>::CONTENDED;
