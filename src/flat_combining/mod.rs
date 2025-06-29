c763db42d4b80f4fe355e627e49855ab36b26636
Alex Gaynor <alex.gaynor@gmail.com>
2025-06-29 16:31:36 -0400

    stress test

--- src/flat_combining/mod.rs
+++ src/flat_combining/mod.rs
@@ -161,27 +161,30 @@ mod tests {
     }
 
     #[test]
-    fn test_contended() {
+    fn test_contended_stress() {
+        const N_ITER: usize = 1024;
         const N_THREADS: usize = 32;
         const N_OPS: usize = 128;
 
-        let m = FlatCombining::new(0);
-        let b = std::sync::Barrier::new(N_THREADS);
+        for _ in 0..N_ITER {
+            let m = FlatCombining::new(0);
+            let b = std::sync::Barrier::new(N_THREADS);
 
-        std::thread::scope(|s| {
-            for _ in 0..N_THREADS {
-                s.spawn(|| {
-                    b.wait();
+            std::thread::scope(|s| {
+                for _ in 0..N_THREADS {
+                    s.spawn(|| {
+                        b.wait();
 
-                    for _ in 0..N_OPS {
-                        m.mutate(|v| *v += 1);
-                    }
-                });
-            }
-        });
+                        for _ in 0..N_OPS {
+                            m.mutate(|v| *v += 1);
+                        }
+                    });
+                }
+            });
 
-        m.mutate(|v| {
-            assert_eq!(*v, N_THREADS * N_OPS);
-        });
+            m.mutate(|v| {
+                assert_eq!(*v, N_THREADS * N_OPS);
+            });
+        }
     }
 }
